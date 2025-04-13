67789fff4b4ce11e07e1dc831be7ac33a051258d
Xavier Briole <xbriole@icloud.com>
2025-04-14 01:57:24 +0200

    Disable trailingSlash in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: config.siteUrl,
+  trailingSlash: "never",
   redirects: {
     "/celebraite/privacy": "/apps/celebraite/privacy",
   },
