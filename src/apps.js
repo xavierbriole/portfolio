2f6b5027447f381df791a3e6a75af4fd32b165b5
Xavier Briole <xbriole@icloud.com>
2025-05-28 21:21:43 +0800

    Updated Celebraite description

--- src/apps.js
+++ src/apps.js
@@ -4,7 +4,7 @@ export default [
     props: {
       appName: "Celebraite",
       description:
-        "Celebraite harnesses the power of artificial intelligence to craft personalized birthday messages, ensuring you never miss a special occasion again.",
+        "Celebraite harnesses the power of artificial intelligence to craft personalized birthday messages",
       iconUrl: "/assets/apps/celebraite.jpg",
       appStoreId: "6471450744",
       platforms: ["iOS", "iPadOS"],
