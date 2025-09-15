0c51cffc3078fa93afd6a36be6deea10582761db
Xavier Briole <xbriole@icloud.com>
2025-09-15 04:40:58 +0200

    Add DraftMind app

--- src/apps.js
+++ src/apps.js
@@ -5,8 +5,19 @@ export default [
       appName: "Celebraite",
       description:
         "Celebraite harnesses the power of artificial intelligence to craft personalized birthday messages",
-      iconUrl: "/assets/apps/celebraite.jpg",
+      iconUrl: "/assets/apps/celebraite.png",
       appStoreId: "6471450744",
+      platforms: ["iOS", "iPadOS"],
+    },
+  },
+  {
+    params: { slug: "draftmind" },
+    props: {
+      appName: "DraftMind",
+      description:
+        "Your personal AI coach that help you pick the best champion for League of Legends",
+      iconUrl: "/assets/apps/draftmind.png",
+      testflightUrl: "https://testflight.apple.com/join/pkQqb4Rq",
       platforms: ["iOS", "iPadOS"],
     },
   },
