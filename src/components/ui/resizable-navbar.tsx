8fe4a294c5a226ddad9051460ffbb465a443bd81
Xavier Briole <xbriole@icloud.com>
2025-09-20 18:19:12 +0200

    Fix colors

--- src/components/ui/resizable-navbar.tsx
+++ src/components/ui/resizable-navbar.tsx
@@ -307,7 +307,7 @@ export const NavbarLogo = ({
   return (
     <a
       href={href || undefined}
-      className="relative z-20 mr-4 flex items-center space-x-2 px-2 py-1 text-sm font-normal text-black dark:text-white"
+      className="relative z-20 mr-4 flex items-center space-x-2 px-2 py-1 text-sm font-normal"
     >
       {children}
     </a>
