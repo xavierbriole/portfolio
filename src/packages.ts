c6505e7cbf7bb458683cbb38a0b2b8639689257e
Xavier Briole <xbriole@icloud.com>
2025-12-10 22:22:06 +0100

    Update assets

--- src/packages.ts
+++ src/packages.ts
@@ -1,12 +1,22 @@
-export default [
+const packages: {
+  params: { slug: string };
+  props: {
+    packageName: string;
+    description: string;
+    iconUrl: string;
+    tags: string[];
+  };
+}[] = [
   {
     params: { slug: "react-cookienotice" },
     props: {
       packageName: "react-cookienotice",
       description:
         "A lightweight & customizable cookie banner for your React App",
-      iconUrl: "/assets/packages/react-cookienotice.jpg",
+      iconUrl: "/assets/packages/react-cookienotice.png",
       tags: ["react"],
     },
   },
 ];
+
+export default packages;
