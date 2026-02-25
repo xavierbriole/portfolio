67e175cc7223265983efc86f64c41b464bcbe3c4
Xavier Briole <xbriole@icloud.com>
2026-02-25 22:06:18 +0100

    Update Riftly appstore link

--- src/apps.ts
+++ src/apps.ts
@@ -27,7 +27,7 @@ const apps: {
       description:
         "Follow your favorite teams and leagues from League of Legends with real-time scores, news and stats",
       iconUrl: "/assets/apps/riftly.png",
-      testflightUrl: "https://testflight.apple.com/join/F1WAmPfH",
+      appStoreId: "6757268977",
       platforms: ["iOS", "iPadOS"],
     },
   },
