439114a54bf3f7c71dd657d0249a3c9ddb842459
Xavier Briole <xbriole@icloud.com>
2026-07-19 13:01:27 +0200

    Remove vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel";
 import react from "@astrojs/react";
 
 // https://astro.build/config
@@ -27,7 +26,6 @@ export default defineConfig({
     sitemap(),
     react(),
   ],
-  adapter: vercel(),
   env: {
     schema: {
       GITHUB_TOKEN: envField.string({
